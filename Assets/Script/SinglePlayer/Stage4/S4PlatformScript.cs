de26a101bb8fd193c76c2148ad12ac4b45aa8220
1114ZE <ycodes.19@gmail.com>
2021-12-12 16:14:30 +0800

    S4 Challenge, wala pa exp.

--- Assets/Script/SinglePlayer/Stage4/S4PlatformScript.cs
+++ Assets/Script/SinglePlayer/Stage4/S4PlatformScript.cs
@@ -9,13 +9,14 @@ public class S4PlatformScript : MonoBehaviour
 
     public string colorValue = "";
     public bool someone = false;
+    public bool wrong;
 
     private void Start()
     {
         Value.GetComponent<TextMeshPro>().text = "4";
         StartCoroutine(changeColor());
     }
-    
+
 
     private void OnTriggerStay(Collider collision)
     {
@@ -26,29 +27,41 @@ public class S4PlatformScript : MonoBehaviour
             {
                 collision.gameObject.GetComponent<S4PlayerData>().MasterPlayerData(gameObject);
                 collision.gameObject.GetComponent<S4PlayerData>().stepped = true;
+                wrong = false;
             }
             else
             {
                 if (!collision.gameObject.GetComponent<S4PlayerData>().stepped)
                 {
-                    gameObject.SetActive(false);
+                    wrong = true;
+                    gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 1000, gameObject.transform.position.z);
+                    StartCoroutine(respawnPlatform());
                 }
             }
             someone = true;
         }
     }
 
+    IEnumerator respawnPlatform()
+    {
+        yield return new WaitForSeconds(5f);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1000, gameObject.transform.position.z);
+
+    }
     private void OnTriggerExit(Collider collision)
     {
         if (collision.gameObject.tag.Equals("Maze") || collision.gameObject.tag.Equals("Zilch") || collision.gameObject.tag.Equals("Trix"))
         {
+
             someone = false;
             collision.gameObject.GetComponent<S4PlayerData>().stepped = false;
-            StartCoroutine(changeColor());
+            if (!wrong)
+            {
+                StartCoroutine(changeColor());
+            }
         }
     }
 
-    
     IEnumerator changeColor()
     {
         while (!someone)
@@ -56,8 +69,8 @@ public class S4PlatformScript : MonoBehaviour
             if (Value.GetComponent<TextMeshPro>().text.Equals("1"))
             {
                 Value.GetComponent<TextMeshPro>().text = "4";
-                Color[] colors = { Color.green, Color.red, Color.blue};
-                string[] colorsStr = { "green", "red", "blue"};
+                Color[] colors = { Color.green, Color.red, Color.blue };
+                string[] colorsStr = { "green", "red", "blue" };
                 int rand = Random.Range(0, colors.Length);
                 colorValue = colorsStr[rand];
                 var goRenderer = GetComponent<Renderer>();
@@ -70,5 +83,5 @@ public class S4PlatformScript : MonoBehaviour
         }
     }
 
-    
+
 }
