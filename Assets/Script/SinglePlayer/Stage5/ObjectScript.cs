fd86c94aca23798efb1d6bf1b367913497794c42
1114ZE <ycodes.19@gmail.com>
2021-12-17 10:57:31 +0800

    before pull master

--- Assets/Script/SinglePlayer/Stage5/ObjectScript.cs
+++ Assets/Script/SinglePlayer/Stage5/ObjectScript.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ObjectScript : MonoBehaviour
 {
     public int Value = 0;
     public float cooldown = 8f;
-    
+    public GameObject point;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +21,6 @@ public class ObjectScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void InitColor()
@@ -30,23 +32,28 @@ public class ObjectScript : MonoBehaviour
         {
             case 0:
                 Value = 1;
+
                 break;
             case 1:
                 Value = 2;
+                // point.GetComponent<TextMeshPro>().text = 2.ToString();
                 break;
             case 2:
                 Value = 4;
+                // point.GetComponent<TextMeshPro>().text = 4.ToString();
                 break;
         }
         var goRenderer = GetComponent<Renderer>();
         goRenderer.material.SetColor("_Color", colors[rand]);
+        point.GetComponent<TextMeshPro>().text = Value.ToString();
+
     }
 
     IEnumerator ChangeColor()
     {
         while (true)
         {
-            if(cooldown <= 0)
+            if (cooldown <= 0)
             {
                 InitColor();
                 cooldown = Random.Range(8f, 13f);
